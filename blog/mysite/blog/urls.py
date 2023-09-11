0e45b3211cc7da712a166416ea9f5416f8adab7d
Krishna Dulal <npu9jc@virginia.edu>
2023-09-11 02:47:27 -0400

    new feature contact, policy added

--- blog/mysite/blog/urls.py
+++ blog/mysite/blog/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('policy.html', views.policy_view, name='policy.html'),
+    path('contact.html', views.contact_view, name='contact.html'),
+    path('submit-contact/', views.submit_contact_form, name='submit_contact'),
+      
 ]
--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from .forms import ContactForm
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -8,3 +10,20 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+def policy_view(request):
+    return render(request, 'policy.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def submit_contact_form(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form data and perform actions (e.g., send an email)
+            # Redirect to a thank you page or display a success message
+            return HttpResponseRedirect('/thank-you/')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
